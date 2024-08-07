5b5a0666370e6e5f048ff179473efecd804a06ed
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-08-07 16:13:18 -0400

    Fix community

--- handlers/default.go
+++ handlers/default.go
@@ -11,6 +11,17 @@ type DefaultHandler struct {
 }
 
 func (h *DefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		h.Home(w, r)
+	case "/community":
+		h.Community(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func (h *DefaultHandler) Home(w http.ResponseWriter, r *http.Request) {
 	data := services.PageData{
 		Title: "Home",
 	}
@@ -18,4 +29,10 @@ func (h *DefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "base.html", data)
 }
 
-// Add more methods for other routes...
+func (h *DefaultHandler) Community(w http.ResponseWriter, r *http.Request) {
+	data := services.PageData{
+		Title: "Community",
+	}
+	tmpl := template.Must(template.ParseFiles("templates/base.html", "templates/community.html", "templates/header.html", "templates/footer.html"))
+	tmpl.ExecuteTemplate(w, "base.html", data)
+}
