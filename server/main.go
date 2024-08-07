944f40921c9cce1ede86369ab4a12e0346183831
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-08-07 15:44:12 -0400

    Create templates

--- server/main.go
+++ server/main.go
@@ -5,10 +5,25 @@ import (
 	"text/template"
 )
 
+type PageData struct {
+	Title string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("public/index.html", "src/header.html", "src/footer.html"))
-		tmpl.ExecuteTemplate(w, "index.html", nil)
+		data := PageData{
+			Title: "Home",
+		}
+		tmpl := template.Must(template.ParseFiles("src/base.html", "public/index.html", "src/header.html", "src/footer.html"))
+		tmpl.ExecuteTemplate(w, "base.html", data)
+	})
+
+	http.HandleFunc("/community", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData{
+			Title: "Community",
+		}
+		tmpl := template.Must(template.ParseFiles("src/base.html", "public/community.html", "src/header.html", "src/footer.html"))
+		tmpl.ExecuteTemplate(w, "base.html", data)
 	})
 
 	// Serve static files
