2d03c9c62838e06bea4d7b39197ccacfceded2d8
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-08-12 16:59:54 -0400

    Create roadmap component

--- components/roadmap_sober.go
+++ components/roadmap_sober.go
@@ -1,48 +1,50 @@
 package components
 
 import (
-	"bytes"
+	"context"
 	"fmt"
 	"io"
 
+	"github.com/a-h/templ"
 	svg "github.com/ajstarks/svgo"
 )
 
-func RoadmapSober(w io.Writer) error {
-	var buf bytes.Buffer
-	canvas := svg.New(&buf)
+func RoadmapSober() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
+		canvas := svg.New(w)
 
-	// Define the dimensions of the SVG canvas
-	width := 800
-	height := 650
+		// Define the dimensions of the SVG canvas
+		width := 800
+		height := 650
 
-	// Start the SVG canvas with the defined dimensions
-	canvas.Startview(width, height, 0, 0, width, height)
-	canvas.Title("Sobriety Roadmaps")
-	canvas.Desc("A roadmap to sobriety")
+		// Start the SVG canvas with the defined dimensions
+		canvas.Startview(width, height, 0, 0, width, height)
+		canvas.Title("Sobriety Roadmaps")
+		canvas.Desc("A roadmap to sobriety")
 
-	// Calculate the center position
-	centerX := width / 2
-	centerY := 0
+		// Calculate the center position
+		centerX := width / 2
+		centerY := 0
 
-	// Apply a translate transformation to center the content
-	canvas.Gtransform(fmt.Sprintf("translate(%d, %d)", centerX, centerY))
+		// Apply a translate transformation to center the content
+		canvas.Gtransform(fmt.Sprintf("translate(%d, %d)", centerX, centerY))
 
-	SVGGroup(canvas, 0, "Admit the Problem")
-	SVGGroup(canvas, 1, "Seek Help")
-	SVGGroup(canvas, 2, "Detoxification")
-	SVGGroup(canvas, 3, "Therapy and Counseling")
-	SVGGroup(canvas, 4, "Support Groups")
-	SVGGroup(canvas, 5, "Healthy Lifestyle")
-	SVGGroup(canvas, 6, "Avoid Triggers")
-	SVGGroup(canvas, 7, "Long-Term Maintenance")
-	SVGGroup(canvas, 8, "Forgive Yourself")
-	SVGGroup(canvas, 9, "Celebrate Milestones")
+		SVGGroup(canvas, 0, "Admit the Problem")
+		SVGGroup(canvas, 1, "Seek Help")
+		SVGGroup(canvas, 2, "Detoxification")
+		SVGGroup(canvas, 3, "Therapy and Counseling")
+		SVGGroup(canvas, 4, "Support Groups")
+		SVGGroup(canvas, 5, "Healthy Lifestyle")
+		SVGGroup(canvas, 6, "Avoid Triggers")
+		SVGGroup(canvas, 7, "Long-Term Maintenance")
+		SVGGroup(canvas, 8, "Forgive Yourself")
+		SVGGroup(canvas, 9, "Celebrate Milestones")
 
-	// End the group transformation
-	canvas.Gend()
+		// End the group transformation
+		canvas.Gend()
 
-	canvas.End()
-	_, err := w.Write(buf.Bytes())
-	return err
+		canvas.End()
+
+		return nil
+	})
 }
