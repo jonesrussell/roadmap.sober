92a100372488897fb3c5f5ba14189855cec990b4
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-08-13 08:56:53 +0000

    Add tailwind

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,7 +3,11 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["../views/**/*.{templ,go}"],
+  content: [
+    "../views/**/*.{templ,go}",
+    "../content/*.{templ,go}",
+    "../ui/*.{templ,go}",
+  ],
   theme: {
     extend: {
       fontFamily: {
